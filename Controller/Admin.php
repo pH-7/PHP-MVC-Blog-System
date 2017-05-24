a3a52a9fe0b70350ebb404a7de5867620577fdef
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-05-24 19:11:29 +0100

    Dont check password in the model

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -10,25 +10,23 @@ namespace TestProject\Controller;
 
 class Admin extends Blog
 {
-
     public function login()
     {
         if ($this->isLogged())
             header('Location: ' . ROOT_URL . '?p=blog&a=all');
 
-        if (isset($_POST['email'], $_POST['password']))
-        {
+        if (isset($_POST['email'], $_POST['password'])) {
             $this->oUtil->getModel('Admin');
             $this->oModel = new \TestProject\Model\Admin;
 
-            if ($this->oModel->login($_POST['email'], $_POST['password']))
-            {
+            $sDbPassword =  $this->oModel->login($_POST['email']);
+            if (password_verify($sDbPassword, $_POST['email'])) {
                 $_SESSION['is_logged'] = 1; // Admin is logged now
                 header('Location: ' . ROOT_URL . '?p=blog&a=all');
                 exit;
-            }
-            else
+            } else {
                 $this->oUtil->sErrMsg = 'Incorrect Login!';
+            }
         }
 
         $this->oUtil->getView('login');
--- Model/Admin.php
+++ Model/Admin.php
@@ -10,15 +10,13 @@ namespace TestProject\Model;
 
 class Admin extends Blog
 {
-
-    public function login($sEmail, $sPassword)
+    public function login($sEmail)
     {
         $oStmt = $this->oDb->prepare('SELECT email, password FROM Admins WHERE email = :email LIMIT 1');
         $oStmt->bindValue(':email', $sEmail, \PDO::PARAM_STR);
         $oStmt->execute();
         $oRow = $oStmt->fetch(\PDO::FETCH_OBJ);
 
-        return password_verify($sPassword, @$oRow->password); // Use the PHP 5.5 password function
+        return @$oRow->password; // Use the PHP 5.5 password function
     }
-
 }
